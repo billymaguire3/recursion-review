d90c38fec20b6f197e9e1b3df368286a595ec859
Arun K <arundas.k.92@gmail.com>
2020-08-17 16:26:21 -0700

    Modified stringifyJSON recursively and edited test file

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj + '';
+  }
+  if (obj === null) {
+    return 'null';
+
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    var result = [];
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      obj.forEach(function(value) {
+        result.push(stringifyJSON(value));
+      });
+      return '[' + result.join(',') + ']';
+    }
+  }
+
+  if (typeof obj === 'object') {
+    var result = [];
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+        continue;
+      }
+      result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    return '{' + result.join(',') + '}';
+  }
 };
