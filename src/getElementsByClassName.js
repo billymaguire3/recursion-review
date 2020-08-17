1711605907d1899180249195cbcebb2501a8d791
Arun K <arundas.k.92@gmail.com>
2020-08-17 13:00:54 -0700

    Implemented getElementsByClassName and corrected the test file

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,20 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var resultArray = [];
+  var helperFunction = function(node) {
+    if (node.classList && node.classList.contains(className)) {
+      resultArray.push(node);
+    }
+
+    _.each(node.childNodes, function(child) {
+      helperFunction(child);
+    });
+  };
+
+  helperFunction(document.body);
+  return resultArray;
 };
+
+
+
